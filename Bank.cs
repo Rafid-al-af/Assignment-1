ee6c329291530034980491a750742b3e458e5402
RAFID-AL-AF <18-38679-3@student.aiub.edu>
2021-03-02 16:40:33 +0600

    update

---
+++ Bank.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Managment_System
+{
+    class Bank
+    {
+        private string name;
+        private Account[] accounts;
+        public Bank(string name, int size)
+        {
+            this.name = name;
+            accounts = new Account[size];
+        }
+        public string Name
+        {
+            set { this.name = value; }
+            get { return this.name; }
+        }
+        public Account[] Accounts
+        {
+            set { this.accounts = value; }
+            get { return this.accounts; }
+        }
+
+        public void PrintAllAccounts()
+        {
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                if (accounts[i] == null)
+                {
+                    continue;
+                }
+                accounts[i].PrintAccount();
+            }
+        }
+        public void AddAccount(Account account)
+        {
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                if (accounts[i] == null)
+                {
+                    accounts[i] = account;
+                    break;
+                }
+            }
+        }
+        public void deleteAccount(int accountNo)
+        {
+            int flag = 0;
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                if (accounts[i] == null)
+                {
+                    continue;
+                }
+                else if (accounts[i].AccountNumber == accountNo)
+                {
+                    accounts[i].PrintAccount();
+                    flag = 0;
+                    break;
+                }
+                else
+                {
+                    flag = 1;
+
+                }
+            }
+            if (flag == 1)
+                Console.WriteLine("Account Not Found");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,12 @@ namespace Bank_Managment_System
     {
         static void Main(string[] args)
         {
+            Bank MyBank = new Bank("Developer's bank", 5);
+            MyBank.AddAccount(new Account(1, "Rafid", 5000, new Address(4, 10, "Dhaka", "Bangladesh")));
+            MyBank.AddAccount(new Account(3003, "Sajib", 5000, new Address(5, 12, "Sylhet", "Bangladesh")));
+            MyBank.AddAccount(new Account(1002, "Anik", 3000, new Address(7, 20, "Chittagong", "Bangladesh")));
+            MyBank.PrintAllAccounts();
+            //ourBank.deletAccount(3001);
         }
     }
 }
