4cdc23e281216bfd7906c518e1b251a28a4ef79e
RAFID-AL-AF <18-38679-3@student.aiub.edu>
2021-03-02 11:28:14 +0600

    update

---
+++ Account.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Managment_System
+{
+    class Account
+    {
+        private int accountNumber;
+        private string accountName;
+        private double balance;
+        private Address address;
+
+        public Account(int accountNumber,string accountname,double balance,Address address)
+        {
+            this.accountNumber = accountNumber;
+            this.accountName = accountName;
+            this.balance = balance;
+            this.address = address;
+
+        }
+
+        public int AccountNumber
+        {
+            set { this.accountNumber = value; }
+            get { return this.accountNumber; }
+        }
+
+        public string AccountName
+        {
+            get { return this.accountName; }
+            set { this.accountName = value; }
+        }
+        public double Balance
+        {
+            get { return this.balance; }
+            set { this.balance = value; }
+        }
+
+        public Address Address
+        {
+            get { return this.address; }
+            set { this.address = value; }
+        }
+
+        public void PrintAccount()
+        {
+            //Console.WriteLine("Account No:"+this.accountNumber+"\nAccount Name:"+this.accountName+"\nBalance:"+this.balance);
+            Console.WriteLine("Account No:{0}\nAccount Name:{1}\nBalance:{2}", this.accountNumber, this.accountName, this.balance);
+            this.address.PrintAddress();
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > this.balance)
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+            else
+            {
+                this.balance = this.balance - amount;
+                Console.WriteLine("Withdrwa Successful");
+                Console.WriteLine("Account No:{0}\nAccount Name:{1}\nBalance:{2}", this.accountNumber, this.accountName, this.balance);
+            }
+        }
+
+        public void Deposit(double amount)
+        {
+            this.balance = this.balance + amount;
+            Console.WriteLine("Deposit Successful");
+            Console.WriteLine("Account No:{0}\nAccount Name:{1}\nBalance:{2}", this.accountNumber, this.accountName, this.balance);
+        }
+
+        public void Transfer(Account account, double amount)
+        {
+            Withdraw(amount);
+            account.Deposit(amount);
+        }
+    }
+}
